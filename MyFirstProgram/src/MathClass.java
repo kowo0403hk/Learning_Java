0529fc658a46a3dcf9a49b1aecb62aff3dbde193
Alex Cheung <alexc.sfu@gmail.com>
2022-09-09 00:31:20 -0700

    added MathClass.java

---
+++ MyFirstProgram/src/MathClass.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MathClass {
+  public static void main(String[] args) throws Exception {
+    double x;
+    double y;
+    double z;
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter side x: ");
+
+    x = scanner.nextDouble();
+
+    System.out.println("Enter side y: ");
+
+    y = scanner.nextDouble();
+
+    z = Math.sqrt((x * x) + (y * y));
+
+    System.out.println("The hypotenuse is: " + z);
+
+    scanner.close();
+  }
+}
