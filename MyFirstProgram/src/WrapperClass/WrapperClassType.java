ed867b17dfea32c704d3083e5e9f04bfc05e1ccf
Alex Cheung <alexc.sfu@gmail.com>
2022-09-10 00:48:51 -0700

    added wrapper class

---
+++ MyFirstProgram/src/WrapperClass/WrapperClassType.java
@@ -0,0 +1,21 @@
+public class WrapperClassType {
+  public static void main(String[] args) {
+    // wrapper class = provieds a way to use primitive data types as reference data
+    // types.
+    // reference data types contain useful methods can be used with collections
+    // (e.g. ArrayList)
+
+    // primitive // wrapper
+    // --------- // -------
+    // boolean // Boolean
+    // char // Character
+    // int // Integer
+    // double // Double
+
+    // autoboxing = the automatic conversion that the Java compiler makes between
+    // the primitive type and the wrapper
+
+    // unboxing = the reverse of autoboxing. Automatic conversion of wrapper class
+    // to primitive
+  }
+}
