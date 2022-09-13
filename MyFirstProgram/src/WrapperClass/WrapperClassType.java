ce63dbb40004edc90c9fc95a9cb89a1db1284a26
Alex Cheung <alexc.sfu@gmail.com>
2022-09-12 23:48:37 -0700

    updated wrapperclass.java

--- MyFirstProgram/src/WrapperClass/WrapperClassType.java
+++ MyFirstProgram/src/WrapperClass/WrapperClassType.java
@@ -12,10 +12,35 @@ public class WrapperClassType {
     // int // Integer
     // double // Double
 
+    // Same idea as String or Number or Array or Object in Javascript
+
     // autoboxing = the automatic conversion that the Java compiler makes between
-    // the primitive type and the wrapper
+    // the primitive type and the corresponding wrapper class
+
+    Boolean a = true; // autoboxing (kinda like type inference?)
+
+    Character b = '@';
+
+    Integer c = 123;
+
+    Double d = 10.33;
+
+    String e = "Alex";
+
+    // wrapper class methods
+    a.booleanValue();
+    c.valueOf(0);
 
     // unboxing = the reverse of autoboxing. Automatic conversion of wrapper class
     // to primitive
+    if (a == true) {
+      // even though variable a is a wrapper class variable, but Java will still go
+      // through the unboxing process and get the primitive value
+      System.out.println("a is true");
+    }
+
+    if (b == '@') {
+      System.out.println("b is a character");
+    }
   }
 }
