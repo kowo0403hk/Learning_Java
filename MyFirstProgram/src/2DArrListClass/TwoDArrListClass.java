adc6e6d5d3088413e923fc1a99fe240aa2720527
Alex Cheung <alexc.sfu@gmail.com>
2022-09-13 23:46:28 -0700

    added 2DArrayList.java

---
+++ MyFirstProgram/src/2DArrListClass/TwoDArrListClass.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class TwoDArrListClass {
+
+  public static void main(String[] args) {
+    // 2D ArrayList = a dynamic list of lists
+    // You can change the size of these lists during runtime
+    ArrayList<String> bakeryList = new ArrayList<>();
+
+    bakeryList.add("Pasta");
+    bakeryList.add("Garlic Bread");
+    bakeryList.add("Donuts");
+
+    System.out.println(bakeryList);
+
+    ArrayList<String> produceList = new ArrayList<>();
+
+    produceList.add("Tomatoes");
+    produceList.add("Zucchini");
+    produceList.add("Peppers");
+
+    System.out.println(produceList);
+
+    ArrayList<String> drinksList = new ArrayList<>();
+
+    drinksList.add("Soda");
+    drinksList.add("Coffee");
+    drinksList.add("Beer");
+
+    System.out.println(drinksList);
+  }
+}
