19940bc4d0605f1724338f77a6a3bfd8861ae1c1
Alex Cheung <alexc.sfu@gmail.com>
2022-09-16 22:57:30 -0700

    added FinalKeyWord.java

---
+++ MyFirstProgram/src/FinalKeyWord/FinalKeyWord.java
@@ -0,0 +1,12 @@
+public class FinalKeyWord {
+  public static void main(String[] args) {
+    // anything that is declared by the final keyword cannot be changed or updated
+    // later on
+
+    // it is a common convention to have your variable name in all caps if it is
+    // final
+    final double PI = 3.14159;
+
+    System.out.println(PI);
+  }
+}
