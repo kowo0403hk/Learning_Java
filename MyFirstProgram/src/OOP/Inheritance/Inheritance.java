5c5409b06e8f5659d0525c0a5ace6a1997c4331f
Alex Cheung <alexc.sfu@gmail.com>
2022-09-22 01:43:20 -0700

    added Inheritance.java

---
+++ MyFirstProgram/src/OOP/Inheritance/Inheritance.java
@@ -0,0 +1,20 @@
+public class Inheritance {
+  public static void main(String[] args) {
+    // inheritance = the process where one class acquires the attributes and methods
+    // of another
+
+    Car car = new Car();
+
+    car.go();
+
+    Bicycle bike = new Bicycle();
+
+    bike.stop();
+
+    System.out.println(car.speed);
+    System.out.println(bike.speed);
+
+    System.out.println(car.doors);
+    System.out.println(bike.pedals);
+  }
+}
