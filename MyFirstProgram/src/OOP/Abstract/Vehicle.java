26c4915903fcc06149dac6a11ffd57e0a790ad74
Alex Cheung <alexc.sfu@gmail.com>
2022-10-03 23:57:12 -0700

    minor update

--- MyFirstProgram/src/OOP/Abstract/Vehicle.java
+++ MyFirstProgram/src/OOP/Abstract/Vehicle.java
@@ -1,4 +1,8 @@
 public abstract class Vehicle {
   // adding the abstract keyword before class keyword makes the class an abstract
   // class.
+
+  abstract go(); // added an abstract method --> it does not have body (curly braces)
+  // the abstract method forces a developer to implment this abstract method the
+  // child class of this parent class
 }
