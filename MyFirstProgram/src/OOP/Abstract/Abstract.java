6624b05cd7a476c194a25951b850729d9ba3280f
Alex Cheung <alexc.sfu@gmail.com>
2022-10-03 23:53:05 -0700

    added Abstract.java

---
+++ MyFirstProgram/src/OOP/Abstract/Abstract.java
@@ -0,0 +1,8 @@
+public class Abstract {
+  public static void main(String[] args) {
+    // abstract = abstract class cannot be instantiated, but it can have a subclass
+    // abstract methods are declaraed without implementation
+
+    Car car = new Car();
+  }
+}
---
+++ MyFirstProgram/src/OOP/Abstract/Vehicle.java
@@ -0,0 +1,4 @@
+public abstract class Vehicle {
+  // adding the abstract keyword before class keyword makes the class an abstract
+  // class.
+}
