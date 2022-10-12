2c353b315bb322e6df6133936213bdfc1fa6d1d7
Alex Cheung <alexc.sfu@gmail.com>
2022-10-11 22:21:36 -0700

    updated AccessModifier.java

--- MyFirstProgram/src/OOP/AccessModifiers/AccessModifiers.java
+++ MyFirstProgram/src/OOP/AccessModifiers/AccessModifiers.java
@@ -1,5 +1,19 @@
 public class AccessModifiers {
   // access modifiers add security to our code
-  // public, protected, private
+  // public, protected, private, no modifier
+
+  /**
+   * @Access Levels
+   * 
+   *         Modifier || Class || Package || Subclass || Global
+   * 
+   *         public || Yes || Yes || Yes || Yes
+   * 
+   *         protected || Yes || Yes || Yes || No
+   * 
+   *         private || Yes || No || No || No
+   * 
+   *         no modifier || Yes || Yes || No || No
+   */
 
 }
